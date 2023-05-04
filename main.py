6db69395f3c9f4f257736c1e3cf17eef9d8cd2f7
Jaeho Jung <jaeho.jung.01@gmail.com>
2023-05-04 17:52:53 +0900

    변수명 영어로 변경하기

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from pyproj import CRS
 from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 
+plt.rcParams['font.family'] = 'NanumGothic'
+plt.rcParams['font.size'] = 12
 
 # Set the max_columns option to None
 pd.set_option('display.max_columns', None)
@@ -279,109 +281,198 @@ gdf = points.to_crs(new_crs)
 map_df = subset
 joined_df = gpd.sjoin(map_df, gdf, how='inner', op='contains')
 
+# joined_df_columns = joined_df.columns
+# for col in joined_df_columns:
+#     print(col)
 
-##################################################
-# # plot points and polygon (simple overlay)
-# fig, ax = plt.subplots(figsize=(10,10))
+print(joined_df['설립구분'].unique())
+print(joined_df['학교특성'].unique())
+print(joined_df['설립유형'].unique())
+print(joined_df['주야구분'].unique())
+
+joined_df['private'] = joined_df['print(joined_df['설립구분'].unique())'].apply(lambda x: 1 if x == '사립' else 0)
+joined_df['private'] = joined_df['print(joined_df['설립구분'].unique())'].apply(lambda x: 1 if x == '사립' else 0)
+joined_df['private'] = joined_df['print(joined_df['설립구분'].unique())'].apply(lambda x: 1 if x == '사립' else 0)
+joined_df['private'] = joined_df['print(joined_df['설립구분'].unique())'].apply(lambda x: 1 if x == '사립' else 0)
+
+# joined_df = joined_df.drop(columns=['학교급코드', '제외여부', '제외사유', '학교특성', '_merge', '전화번호', '팩스번호'])
+# print(joined_df['private'].unique())
 #
-# polygons.plot(ax=ax, alpha=0.5, edgecolor='black')
-# joined.plot(ax=ax, color='red', markersize=5)
+# grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
+# genders = ['남']
 #
-# plt.show()
+# boy_cols = [f"{g}(남)" for g in grades]
+# girl_cols = [f"{g}(여)" for g in grades]
+# boy_ratio = joined_df[boy_cols].describe()
+# girl_ratio = joined_df[girl_cols].describe()
+#
+#
+# # Define a mapping of Korean column names to English column names
+# columns_mapping = {
+#     "OBJECTID": "object_id",
+#     "HAKGUDO_ID": "hakgudo_id",
+#     "HAKGUDO_NM": "hakgudo_name",
+#     "HAKGUDO_GB": "hakgudo_type",
+#     "SD_CD": "sd_code",
+#     "SGG_CD": "sgg_code",
+#     "EDU_UP_CD": "edu_up_code",
+#     "EDU_UP_NM": "edu_up_name",
+#     "EDU_CD": "edu_code",
+#     "EDU_NM": "edu_name",
+#     "geometry": "geometry",
+#     "index_right": "index_right",
+#     "시도교육청": "sido_edu_office",
+#     "지역교육청": "local_edu_office",
+#     "지역": "region",
+#     "정보공시 학교코드": "school_code",
+#     "학교명": "school_name",
+#     "설립구분": "foundation_type",
+#     "1학년(남)": "1st_grade_male",
+#     "1학년(여)": "1st_grade_female",
+#     "2학년(남)": "2nd_grade_male",
+#     "2학년(여)": "2nd_grade_female",
+#     "3학년(남)": "3rd_grade_male",
+#     "3학년(여)": "3rd_grade_female",
+#     "4학년(남)": "4th_grade_male",
+#     "4학년(여)": "4th_grade_female",
+#     "5학년(남)": "5th_grade_male",
+#     "5학년(여)": "5th_grade_female",
+#     "6학년(남)": "6th_grade_male",
+#     "6학년(여)": "6th_grade_female",
+#     "특수학급(남)": "special_class_male",
+#     "특수학급(여)": "special_class_female",
+#     "순회학급(남)": "mobile_class_male",
+#     "순회학급(여)": "mobile_class_female",
+#     "계(남)": "total_male",
+#     "계(여)": "total_female",
+#     "총계": "total",
+#     "설립유형": "foundation_type",
+#     "주야구분": "day_night_division",
+#     "개교기념일": "foundation_anniversary",
+#     "설립일": "foundation_date",
+#     "법정동코드": "legal_dong_code",
+#     "주소내역": "address_detail",
+#     "상세주소내역": "address_detail2",
+#     "우편번호": "zip_code",
+#     "학교도로명 우편번호": "road_address_zip_code"}
 
-# count the number of points for each polygon
-counts = joined.index.value_counts()
 
-# select polygons with more than one point
-multi_point_polygons = counts[counts > 1].index.tolist()
-
-# print the list of polygons with more than one point
-print(multi_point_polygons)
-
-# generate boys/ratio info of each grade
-grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
-
-for grade in grades:
-    total_column = f"{grade}(합계)"
-    columns_to_sum = [f"{grade}(남)", f"{grade}(여)"]
-    joined_df[total_column] = joined_df[columns_to_sum].sum(axis=1)
-
-grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
-genders = ['남']
-
-for grade in grades:
-    for gender in genders:
-        column_name = f"{grade}({gender})"
-        column_name_denominator = f"{grade}(합계)"
-        joined_df[column_name + '비율'] = joined_df[column_name]/joined_df[column_name_denominator]
-
-# # plot boys ratio
-# fig, ax = plt.subplots(figsize=(10, 10))
-# joined_df.plot(column='1학년(남)비율', cmap='Blues', ax=ax, legend=True)
-# plt.show()
-
+# ##################################################
+# # # plot points and polygon (simple overlay)
+# # fig, ax = plt.subplots(figsize=(10,10))
+# #
+# # polygons.plot(ax=ax, alpha=0.5, edgecolor='black')
+# # joined.plot(ax=ax, color='red', markersize=5)
+# #
+# # plt.show()
+#
+# # count the number of points for each polygon
+# counts = joined.index.value_counts()
+#
+# # select polygons with more than one point
+# multi_point_polygons = counts[counts > 1].index.tolist()
+#
+# # print the list of polygons with more than one point
+# print(multi_point_polygons)
+#
+# # generate boys/ratio info of each grade
+# grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
+#
+# for grade in grades:
+#     total_column = f"{grade}(합계)"
+#     columns_to_sum = [f"{grade}(남)", f"{grade}(여)"]
+#     joined_df[total_column] = joined_df[columns_to_sum].sum(axis=1)
+#
+# grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
+# genders = ['남']
+#
 # for grade in grades:
 #     for gender in genders:
-#         plot_name = f"{grade}(남)비율"
-#         fig, ax = plt.subplots(figsize=(10, 10))
-#         joined_df.plot(column=plot_name, cmap='Blues', ax=ax, legend=True)
-#         plt.savefig(f'{plot_name}.png', dpi=300)
-
-# # create a custom colormap
-# cmap = LinearSegmentedColormap.from_list('custom', [(0, 'red'), (0.5, 'white'), (1, 'blue')])
+#         column_name = f"{grade}({gender})"
+#         column_name_denominator = f"{grade}(합계)"
+#         joined_df[column_name + '비율'] = joined_df[column_name]/joined_df[column_name_denominator]
 #
-# # loop through grades and genders and plot the data
-# for grade in grades:
-#     for gender in genders:
-#         plot_name = f"{grade}({gender})비율"
-#         fig, ax = plt.subplots(figsize=(10, 10))
-#         vmin, vmax = joined_df[plot_name].min(), joined_df[plot_name].max()
-#         norm = plt.Normalize(vmin=vmin, vmax=vmax)
-#         joined_df.plot(column=plot_name, cmap=cmap, ax=ax, norm=norm, legend=True)
-#         plt.savefig(f'{plot_name}.png', dpi=100)
-
-
-# sns.set_theme(style="whitegrid")
-# sns.histplot(data=joined_df, x="1학년(남)비율", kde=True)
+# # # plot boys ratio
+# # fig, ax = plt.subplots(figsize=(10, 10))
+# # joined_df.plot(column='1학년(남)비율', cmap='Blues', ax=ax, legend=True)
+# # plt.show()
 #
-# # create the plot
-# fig, ax = plt.subplots()
-# ax.hist(joined_df["1학년(남)비율"], bins=30, density=True)
+# # for grade in grades:
+# #     for gender in genders:
+# #         plot_name = f"{grade}(남)비율"
+# #         fig, ax = plt.subplots(figsize=(10, 10))
+# #         joined_df.plot(column=plot_name, cmap='Blues', ax=ax, legend=True)
+# #         plt.savefig(f'{plot_name}.png', dpi=300)
 #
-# # set labels and title
-# ax.set_xlabel('Value')
-# ax.set_ylabel('Density')
-# ax.set_title('Normal Distribution')
+# # # create a custom colormap
+# # cmap = LinearSegmentedColormap.from_list('custom', [(0, 'red'), (0.5, 'white'), (1, 'blue')])
+# #
+# # # loop through grades and genders and plot the data
+# # for grade in grades:
+# #     for gender in genders:
+# #         plot_name = f"{grade}({gender})비율"
+# #         fig, ax = plt.subplots(figsize=(10, 10))
+# #         vmin, vmax = joined_df[plot_name].min(), joined_df[plot_name].max()
+# #         norm = plt.Normalize(vmin=vmin, vmax=vmax)
+# #         joined_df.plot(column=plot_name, cmap=cmap, ax=ax, norm=norm, legend=True)
+# #         plt.savefig(f'{plot_name}.png', dpi=100)
 #
-# # save the plot to a file
-# plt.savefig('distribution.png', dpi=100)
-
-# Set the font family to one that supports Hangul characters
-plt.rcParams['font.family'] = 'NanumGothic'
-
-sns.set_style("whitegrid")
-
-# create sample data
-data = sns.load_dataset("tips")
-
-# create distribution plot
-sns.kdeplot(data=joined_df["1학년(남)비율"], fill=True)
-
-# add histogram line
-sns.histplot(data=joined_df["1학년(남)비율"], color="grey", alpha=.2, kde=True)
-
-# save plot
-plt.savefig("distribution_plot_with_histogram.png", dpi=300)
-
-
-
-# create histogram plot
-fig, ax = plt.subplots(figsize=(8, 5))
-sns.histplot(data=joined_df["1학년(남)비율"], ax=ax, color="grey", alpha=.2, kde=True)
-
-# add KDE plot on twin Axes
-ax2 = ax.twinx()
-sns.kdeplot(data=joined_df["1학년(남)비율"], shade=True, ax=ax2, color='red')
-ax2.set_ylabel('KDE', color='red')
-ax2.tick_params(axis='y', labelcolor='red')
-plt.show()
+#
+# # sns.set_theme(style="whitegrid")
+# # sns.histplot(data=joined_df, x="1학년(남)비율", kde=True)
+# #
+# # # create the plot
+# # fig, ax = plt.subplots()
+# # ax.hist(joined_df["1학년(남)비율"], bins=30, density=True)
+# #
+# # # set labels and title
+# # ax.set_xlabel('Value')
+# # ax.set_ylabel('Density')
+# # ax.set_title('Normal Distribution')
+# #
+# # # save the plot to a file
+# # plt.savefig('distribution.png', dpi=100)
+#
+# # Set the font family to one that supports Hangul characters
+# plt.rcParams['font.family'] = 'NanumGothic'
+#
+# sns.set_style("whitegrid")
+#
+# # create distribution plot
+# sns.kdeplot(data=joined_df["1학년(남)비율"], fill=True)
+#
+# # add histogram line
+# sns.histplot(data=joined_df["1학년(남)비율"], color="grey", alpha=.2, kde=True)
+#
+# # save plot
+# plt.savefig("distribution_plot_with_histogram.png", dpi=300)
+#
+# # # create histogram plot
+# # fig, ax = plt.subplots(figsize=(8, 5))
+# # sns.histplot(data=joined_df["1학년(남)비율"], ax=ax, color="grey", alpha=.2, kde=True)
+# #
+# # # add KDE plot on twin Axes
+# # ax2 = ax.twinx()
+# # sns.kdeplot(data=joined_df["1학년(남)비율"], shade=True, ax=ax2, color='red')
+# # ax2.set_ylabel('KDE', color='red')
+# # ax2.tick_params(axis='y', labelcolor='red')
+# # plt.show()
+#
+#
+# # # Define your data and groups
+# # grades = ['1학년', '2학년', '3학년', '4학년', '5학년', '6학년']
+# # data = [joined_df[f"{g}(남)비율"] for g in grades]
+# #
+# # # Create a grid of subplots
+# # fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+# # axes = axes.flatten()
+# #
+# # # Loop over each group of data and plot a histogram on the corresponding subplot
+# # for i, (g, d) in enumerate(zip(grades, data)):
+# #     ax = axes[i]
+# #     sns.histplot(data=d, ax=ax, color="grey", alpha=.2, kde=True)
+# #     ax.set_title(g)
+# #
+# # # Adjust spacing between subplots and add a title to the big plot
+# # fig.tight_layout()
+# # fig.suptitle('Histograms by Grade')
