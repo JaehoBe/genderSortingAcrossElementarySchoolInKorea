19672797e55ea11790ae0ba88755dcd963088ebb
Jaeho Jung <jaeho.jung.01@gmail.com>
2023-05-08 19:14:24 +0900

    변수명 영어로 변경하기, number of birth, bot/girl ratio of Korea, add birth data of Seoul(dfSeoulBirth)

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from pyproj import CRS
 from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 from collections import Counter
+import re
 
 plt.rcParams['font.family'] = 'NanumGothic'
 plt.rcParams['font.size'] = 12
@@ -32,7 +33,90 @@ path_engineered_data = os.path.join(base_path, r'engineered_data')
 if not os.path.exists(path_engineered_data):
    os.makedirs(path_engineered_data)
 
+##################################################
+# open files: seoul birth data
+file_name = "data/seoulBirthData/출산순위별+출생_20230508155221.csv"
+file_path = os.path.join(base_path, file_name)
+df = pd.read_csv(file_path, encoding='utf-8', header=[0,1,2])
+
+print(df.head())
+
+# Split the data into df1 and df2
+df1 = data.iloc[:, :2]
+df2 = data.iloc[:, 2:]
+
+df2.columns = df2.columns.droplevel(level=1)
+
+# Translate lower level headers
+df2 = df2.rename(columns={'계': 'total', '남자': 'male', '여자': 'female'})
+
+df2 = df2.drop(index=0)
+
+# Get the upper header
+upper_header = df2.columns.get_level_values(0)
+
+# Get the lower header
+lower_header = df2.columns.get_level_values(1)
+
+# Get the year information
+year_info = upper_header.str.split(" ").str[0]
+
+# Add the year information to the lower header
+new_lower_header = year_info + lower_header
+
+# Assign the new header to the DataFrame
+df2.columns = [upper_header, new_lower_header]
+
+# drop upper header
+df2.columns = df2.columns.droplevel(0)
+
+# drop the first row
+df1 = df1.iloc[1:]
+
+# drop the first column
+df1 = df1.iloc[:, 1:]
+df1.columns = ['bySeoulDistrict']
+df2 = df2.set_index(df1['bySeoulDistrict'])
+df2.loc['SeoulTotal'] = df2.sum(axis=0)
+
+
+total_cols = df2.filter(like='total').columns
+df2_total = df2[total_cols]
+seoul_total = df2.loc['SeoulTotal', total_cols]
+
+male_cols = df2.filter(regex='^(?!.*female).*male$').columns
+df2_male = df2[male_cols]
+seoul_male = df2.loc['SeoulTotal', male_cols]
+
+female_cols = df2.filter(like='female').columns
+df2_female = df2[female_cols]
+seoul_female = df2.loc['SeoulTotal', female_cols]
+
+print(seoul_total)
+print(seoul_male)
+print(seoul_female)
+
+# reset index of each series
+total = seoul_total.reset_index(drop=True)
+male = seoul_male.reset_index(drop=True)
+female = seoul_female.reset_index(drop=True)
+print(total)
+
+# concatenate horizontally
+dfSeoulBirth = pd.concat([total, male, female], axis=1)
+dfSeoulBirth['year'] = range(2000, 2022)
+print(dfSeoulBirth)
+
+dfSeoulBirth.columns = ['SeoulTotal', 'SeoulMaleTotal', 'SeoulFemaleTotal', 'year']
+
+dfSeoulBirth = dfSeoulBirth.reindex(columns=['year', 'SeoulTotal', 'SeoulMaleTotal', 'SeoulFemaleTotal'])
+print(dfSeoulBirth)
+
+
 '''
+
+
+
 ##################################################
 # open files
 
